a5af9ecac9c75703b810f9c99f0f5c732f84b59e
ghamlet <dmtr317744@gmail.com>
2023-12-04 15:37:03 +0300

    'efer'

--- user_task/rsgr5.py
+++ user_task/rsgr5.py
@@ -1,7 +1,9 @@
 import subprocess
 
-cmd = "git add ." # Здесь вместо date Ваша команда для git
-
+#cmd = "git commit -m 'efer' "
+#cmd = "git push"
+cmd  = "git add ."
 returned_output = subprocess.check_output(cmd) # returned_output содержит вывод в виде строки байтов
 
 print('Результат выполнения команды:', returned_output.decode("utf-8")) 
+#dfghjkop;
